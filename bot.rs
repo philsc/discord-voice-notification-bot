f0a1ed51aea55eaf129d49b630f79b240fb1be3e
Philipp Schrader <philipp.schrader@gmail.com>
2022-10-16 16:25:53 -0700

    Make bot killable and start listening to voice channel events

--- bot.rs
+++ bot.rs
@@ -2,7 +2,9 @@ use std::env;
 
 use serenity::async_trait;
 use serenity::prelude::*;
-use serenity::model::channel::Message;
+use serenity::model::channel::{Channel, ChannelType, Message};
+use serenity::model::id::ChannelId;
+use serenity::model::voice::VoiceState;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{StandardFramework, CommandResult};
 
@@ -10,10 +12,47 @@ use serenity::framework::standard::{StandardFramework, CommandResult};
 #[commands(ping)]
 struct General;
 
-struct Handler;
+#[derive(Default)]
+struct Handler {
+    voice_active: bool,
+}
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn voice_state_update(&self, ctx: Context, old: Option<VoiceState>, new: VoiceState) {
+        let id = match new.channel_id {
+            None => return,
+            Some(id) => id,
+        };
+        let channel = match id.to_channel(&ctx).await {
+            Err(why) => {
+                println!("Failed to get channel: {:?}", why);
+                return;
+            },
+            Ok(channel) => channel,
+        };
+        let guild_channel = match channel {
+            Channel::Guild(guild_channel) => guild_channel,
+            _ => return,
+        };
+        if guild_channel.kind != ChannelType::Voice {
+            return;
+        }
+        println!("Got event for channel called \"{}\"", guild_channel.name());
+
+        let member_count = match guild_channel.member_count {
+            Some(count) => count,
+            _ => {
+                println!("No member_count!");
+                return;
+            },
+        };
+
+        if member_count > 0 && !self.voice_active {
+            //self.voice_active = true;
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -25,15 +64,23 @@ async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("token");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
+        .event_handler(Handler::default())
         .framework(framework)
         .await
         .expect("Error creating client");
+
+    let shard_manager = client.shard_manager.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        shard_manager.lock().await.shutdown_all().await;
+    });
 
     // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
+
+    println!("Bot has shut down.");
 }
 
 #[command]
